2ef904fc4bd196525dd70b293ef3ec62f2c269dc
akutsang <akuutsang@gmail.com>
2024-10-30 13:03:47 +0100

    skeleton project with all dependancies

---
+++ youtubeClone/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class' depending on your preference
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
